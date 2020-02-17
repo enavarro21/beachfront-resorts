f12236e5d266bd907844e7eb2e5d14cf2355ec62
Erwin Navarro <erwin.r.navarro1@gmail.com>
2020-02-16 23:06:37 -0800

    Create Featured Rooms display on the Home page with working links and styles

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Title from "./Title";
+import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   // Set up contextType from the Entire Context
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
-    return <div>from featured rooms</div>;
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />
+    })
+    return (
+      <section className="featured-rooms">
+      <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
---
+++ src/components/Room.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// Use a default img for broken images
+import defaultImg from "../images/room-1.jpeg";
+
+// Used to check if the prop has been passed
+import PropTypes from "prop-types";
+
+export default function Room({ room }) {
+  const { name, slug, images, price } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="single-room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
+  );
+}
+
+// Check if the Room object has the specified Props
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired
+  })
+};
