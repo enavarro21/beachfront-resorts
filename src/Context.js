c141cd30f564ae14b6edcc95df87e9838201422f
Erwin Navarro <erwin.r.navarro1@gmail.com>
2020-02-17 18:40:08 -0800

    Finish StyledHero component and SingleRoom page view

--- src/Context.js
+++ src/Context.js
@@ -42,9 +42,16 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  // Filter rooms based on the selected room, and make availabel in context
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
